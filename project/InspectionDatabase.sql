acff5481d050ceedfe74a1ce1d31e8dcb3702965
Kelim <k.almousa@eggsperience.biz>
2019-11-27 12:12:38 +0300

    veritabanı ve veritabanı ER modeli oluşturuldu

--- project/InspectionDatabase.sql
+++ project/InspectionDatabase.sql
@@ -18,8 +18,6 @@
 --
 -- Table structure for table `ders`
 --
-create database if not exists `Ispection`;
-use Inspection
 
 DROP TABLE IF EXISTS `ders`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -35,53 +33,11 @@ CREATE TABLE `ders` (
   `ders_hoca_id` int(10) unsigned DEFAULT NULL,
   `ders_salon` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`ders_id`),
-  UNIQUE KEY `ders_UN` (`ders_id`)
+  UNIQUE KEY `ders_UN` (`ders_id`),
+  KEY `ders_FK` (`ders_hoca_id`),
+  CONSTRAINT `ders_FK` FOREIGN KEY (`ders_hoca_id`) REFERENCES `hoca` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `devamsizlik`
---
-
-DROP TABLE IF EXISTS `devamsizlik`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `devamsizlik` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `ogr_id` int(10) unsigned DEFAULT NULL,
-  `ders_id` int(10) unsigned DEFAULT NULL,
-  `islem_tarihi` varchar(100) DEFAULT NULL,
-  `devamsizlik_kalma` tinyint(1) DEFAULT NULL,
-  `islem_location` varchar(100) DEFAULT NULL,
-  `islem_foto` blob,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `devamsizlik_UN` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `ogrenci`
---
-
-DROP TABLE IF EXISTS `ogrenci`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `ogrenci` (
-  `ogr_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `ogr_no` bigint(20) unsigned NOT NULL,
-  `ogr_name` varchar(100) NOT NULL,
-  `ogr_surname` varchar(100) DEFAULT NULL,
-  `ogr_user_id` int(10) unsigned DEFAULT NULL,
-  `ogr_username` varchar(100) DEFAULT NULL,
-  `ogr_ogrenim_turu` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`ogr_id`),
-  UNIQUE KEY `ogrenci_UN` (`ogr_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping routines for database 'Inspection'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -92,4 +48,4 @@ CREATE TABLE `ogrenci` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-11-25 17:15:13
+-- Dump completed on 2019-11-27 12:11:10
